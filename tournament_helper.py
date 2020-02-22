480bc05ba91f16eb4b95fa67f3579d014abe8a38
Logan Gore <gorel@fb.com>
2020-02-22 13:14:00 -0800

    Add tournament commands

---
+++ tournament_helper.py
@@ -0,0 +1,55 @@
+import json
+import random
+
+from typing import List
+
+
+TOURNAMENT_DATA_JSON = "tournament_data.json"
+
+
+def _init():
+    if not os.path.exists(TOURNAMENT_DATA_JSON):
+        with open(TOURNAMENT_DATA_JSON, "w") as f:
+            f.write("{}")
+
+
+def new_tournament(guild_id: int, team_size: int) -> None:
+    data = {
+        "team_size": team_size,
+        "players": [],
+    }
+    with open(TOURNAMENT_DATA_JSON) as f:
+        tournament_data = json.load(f)
+
+    tournament_data[str(guild_id)] = data
+    with open(TOURNAMENT_DATA_JSON, "w") as f:
+        json.dump(tournament_data, f)
+
+
+def join_tournament(guild_id: int, discord_id: int) -> None:
+    # TODO: Should probably return success or failure
+    with open(TOURNAMENT_DATA_JSON) as f:
+        tournament_data = json.load(f)
+    players = tournament_data[str(guild_id)]["players"]
+    if discord_id not in players:
+        players.append(discord_id)
+    with open(TOURNAMENT_DATA_JSON) as f:
+        json.dump(tournament_data, f)
+
+
+def start_tournament(guild_id: int) -> List[List[int]]:
+    with open(TOURNAMENT_DATA_JSON) as f:
+        tournament_data = json.load(f)
+    players = tournament_data[str(guild_id)]["players"]
+    team_size = tournament_data[str(guild_id)]["team_size"]
+
+    random.shuffle(players)
+    teams: List[List[int]] = []
+    for i in range(0, len(players), team_size):
+        # TODO: If a team can't be filled properly, add bots or something
+        next_team = players[i : i + team_size]
+        teams.append(next_team)
+    return teams
+
+
+_init()

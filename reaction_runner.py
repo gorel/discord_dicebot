2663653c6596bf56559b7597bf57ef2465cc77a6
Logan Gore <gorel@fb.com>
2021-09-25 09:31:20 -0700

    [untested] Reactions directory + new ban reaction feature

--- reaction_runner.py
+++ reaction_runner.py
@@ -1,56 +1,20 @@
 #!/usr/bin/env python3
 
-import asyncio
-import enum
 import logging
 from typing import Awaitable, Callable, List, Optional
 
 import discord
 
-import db_helper
-from commands import ban
 from message_context import MessageContext
 from models import DiscordUser, HandlerStatus, Status, Time
+from reactions import ban
 
 
 ReactionFunc = Callable[[discord.Reaction, MessageContext], Awaitable[HandlerStatus]]
 
 
-# TODO: Move this into its own file
-async def handle_ban_reaction(
-    reaction: discord.Reaction, ctx: MessageContext,
-) -> HandlerStatus:
-    is_ban_emoji = not isinstance(reaction.emoji, str) and reaction.emoji.name == "BAN"
-    if not is_ban_emoji or reaction.count != 2:
-        return HandlerStatus(Status.Invalid)
-
-    # Check if this message has been banned before
-    banned_before = db_helper.has_message_been_banned(
-        ctx.db_conn, reaction.message.guild.id, reaction.message.id
-    )
-    if banned_before:
-        logging.warning("New ban reaction on message but it was banned before.")
-        return HandlerStatus(Status.Invalid)
-
-    # We need to record that this message has now been banned
-    db_helper.record_banned_message(
-        ctx.db_conn, reaction.message.guild.id, reaction.message.id
-    )
-
-    await reaction.message.channel.send("Bro", reference=reaction.message)
-    # Sleep 3 seconds to build suspense
-    await asyncio.sleep(3)
-    await ban.ban(
-        ctx,
-        target=DiscordUser(reaction.message.author.id),
-        timer=Time("1hr"),
-        ban_as_bot=True,
-    )
-    return HandlerStatus(Status.Success)
-
-
 DEFAULT_REACTION_HANDLERS = [
-    handle_ban_reaction,
+    ban.handle_ban_reaction,
 ]
 
 

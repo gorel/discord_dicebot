cf788b0f86f368e370bbfab124572b4ccdf07593
Logan Gore <gorel@fb.com>
2021-12-29 12:26:27 -0500

    Use aiohttp and set media_filter for GIFs

--- commands/giffer.py
+++ commands/giffer.py
@@ -7,25 +7,31 @@ import random
 import requests
 from typing import List, Optional
 
+import aiohttp
+
 from message_context import MessageContext
 from models import GreedyStr
+
 
 class TenorGifRetriever:
     def __init__(self, api_key: Optional[str] = None) -> None:
         self.api_key = api_key or os.getenv("TENOR_API_KEY")
 
-    def get(self, q: str) -> List[str]:
+    async def get(self, q: str) -> List[str]:
         url = "https://g.tenor.com/v1/search"
         params = {
             "q": q,
             "key": self.api_key,
             "locale": "en_US",
             "content_filter": "low",
+            "media_filter": "basic",
         }
         try:
-            response = requests.get(url, params=params)
-            top_gifs = json.loads(response.content)["results"]
-            return [gif["url"] for gif in top_gifs]
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url, params=params) as response:
+                    json_resp = await response.json()
+                    top_gifs = json_resp["results"]
+                    return [gif["url"] for gif in top_gifs]
         except Exception:
             return []
 
@@ -33,7 +39,7 @@ class TenorGifRetriever:
 async def gif(ctx: MessageContext, q: GreedyStr) -> None:
     """Retrieve a random GIF from Tenor when searching the query string"""
     retriever = TenorGifRetriever()
-    urls = retriever.get(q)
+    urls = await retriever.get(q)
     if len(urls) == 0:
         logging.warning(f"Could not find any GIFs for query {q}")
         await ctx.channel.send("Could not find any GIFs for that query :(")

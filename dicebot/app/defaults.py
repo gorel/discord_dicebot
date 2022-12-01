de21e964889f1738b89b99df6578975ba3d22143
Logan Gore <gorel@meta.com>
2022-12-01 14:50:47 -0800

    Celery works!

--- dicebot/app/defaults.py
+++ dicebot/app/defaults.py
@@ -3,4 +3,4 @@
 DEFAULT_DB_URI = "sqlite+aiosqlite:///:memory:"
 DEFAULT_BROKER_URL = "redis://localhost:6379"
 DEFAULT_RESULT_BACKEND = "redis://localhost:6379"
-TASKS_DIR = "dicebot.data.tasks"
+TASKS_DIR = "dicebot.tasks"
---
+++ dicebot/tasks/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from dicebot.core.import_witchcraft import import_submodules
+
+# Pull in all tasks under this directory
+import_submodules("dicebot.tasks")

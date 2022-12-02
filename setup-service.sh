f1bf04e4829d84b06fc8b42501f147a126c824e3
Logan Gore <gorel@meta.com>
2022-12-02 15:34:05 -0800

    Minor update to compose

--- setup-service.sh
+++ setup-service.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# First copy the compose template to systemd
 ln -nsf /etc/systemd/system/docker-compose@.service ./compose/docker-compose@.service
+# Then copy our docker-compose to the directory with compose files
 ln -nsf /etc/docker/compose/dicebot ./docker-compose.yml
+# And start the docker-compose service
 systemctl start docker-compose@

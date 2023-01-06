23fc0cc34fb3fe1e26c58d41a0b4e3b884e234d8
Logan Gore <lgore93@gmail.com>
2023-01-06 16:30:32 -0500

    test_reset_roll

--- dicebot/commands/test/test_reset_roll.py
+++ dicebot/commands/test/test_reset_roll.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 
+import asyncio
 import unittest
 
+from dicebot.commands import reset_roll
+from dicebot.test.utils import TestMessageContext
 
-class TestResetRoll(unittest.TestCase):
+
+class TestResetRoll(unittest.IsolatedAsyncioTestCase):
+    async def asyncSetUp(self) -> None:
+        await super().asyncSetUp()
+        asyncio.get_running_loop().set_debug(False)
+
     async def test_reset_roll(self) -> None:
-        pass
+        # Arrange
+        ctx = TestMessageContext.get()
+        # Act
+        await reset_roll.reset_roll(ctx, 123)
+        # Assert
+        self.assertEqual(123, ctx.guild.current_roll)
+        ctx.session.commit.assert_awaited_once()
+        ctx.channel.send.assert_awaited_once()

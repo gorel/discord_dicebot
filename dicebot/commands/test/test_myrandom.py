a3afe811c867cf413247c33f2885d11c129e4767
Logan Gore <lgore93@gmail.com>
2023-01-06 16:22:59 -0500

    test_myrandom

--- dicebot/commands/test/test_myrandom.py
+++ dicebot/commands/test/test_myrandom.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 
+import asyncio
 import unittest
 
+from dicebot.commands import myrandom
+from dicebot.data.types.greedy_str import GreedyStr
+from dicebot.test.utils import TestMessageContext
 
-class TestMyRandom(unittest.TestCase):
+
+class TestMyRandom(unittest.IsolatedAsyncioTestCase):
+    async def asyncSetUp(self) -> None:
+        await super().asyncSetUp()
+        asyncio.get_running_loop().set_debug(False)
+
     async def test_choice(self) -> None:
-        pass
+        # Arrange
+        ctx = TestMessageContext.get()
+        # Act
+        await myrandom.choice(ctx, GreedyStr("a b c"))
+        # Assert
+        ctx.quote_reply.assert_awaited_once()

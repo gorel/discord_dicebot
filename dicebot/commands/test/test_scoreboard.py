32856c55450128ca81b587e23fa6fb9ed7fb0c23
Logan Gore <lgore93@gmail.com>
2023-01-06 16:34:06 -0500

    test_scoreboard

--- dicebot/commands/test/test_scoreboard.py
+++ dicebot/commands/test/test_scoreboard.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 
+import asyncio
 import unittest
 
+from dicebot.commands import scoreboard
+from dicebot.test.utils import TestMessageContext
 
-class TestScoreboard(unittest.TestCase):
-    def test_scoreboard(self) -> None:
-        pass
+
+class TestScoreboard(unittest.IsolatedAsyncioTestCase):
+    async def asyncSetUp(self) -> None:
+        await super().asyncSetUp()
+        asyncio.get_running_loop().set_debug(False)
+
+    async def test_scoreboard(self) -> None:
+        # Arrange
+        ctx = TestMessageContext.get()
+        # Act
+        await scoreboard.scoreboard(ctx)
+        # Assert
+        ctx.guild.roll_scoreboard_str.assert_awaited_once()
+        ctx.channel.send.assert_awaited_once()

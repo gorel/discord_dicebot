14f618956976db1c3116c267ea69170c687f0131
Logan Gore <lgore93@gmail.com>
2023-01-06 16:36:07 -0500

    test_web

--- dicebot/commands/test/test_web.py
+++ dicebot/commands/test/test_web.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 
+import asyncio
 import unittest
 
+from dicebot.commands import web
+from dicebot.test.utils import TestMessageContext
 
-class TestWeb(unittest.TestCase):
-    def test_web(self) -> None:
-        pass
+
+class TestWeb(unittest.IsolatedAsyncioTestCase):
+    async def asyncSetUp(self) -> None:
+        await super().asyncSetUp()
+        asyncio.get_running_loop().set_debug(False)
+
+    async def test_web(self) -> None:
+        # Arrange
+        ctx = TestMessageContext.get()
+        # Act
+        await web.web(ctx)
+        # Assert
+        ctx.channel.send.assert_awaited_once()

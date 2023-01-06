03f03c1a2c75e6ed7de579dcc284bcac492dae7a
Logan Gore <lgore93@gmail.com>
2023-01-06 11:31:11 -0500

    test_clear_stats

--- dicebot/commands/test/test_clear_stats.py
+++ dicebot/commands/test/test_clear_stats.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 
+import asyncio
 import unittest
 
+from dicebot.commands import clear_stats
+from dicebot.test.utils import TestMessageContext
 
-class TestClearStats(unittest.TestCase):
+
+class TestClearStats(unittest.IsolatedAsyncioTestCase):
+    async def asyncSetUp(self) -> None:
+        await super().asyncSetUp()
+        asyncio.get_running_loop().set_debug(False)
+
     async def test_clear_stats(self) -> None:
-        pass
+        # Arrange
+        ctx = TestMessageContext.get()
+        # Act
+        await clear_stats.clear_stats(ctx)
+        # Assert
+        ctx.guild.clear_stats.assert_awaited_once_with(ctx.session)
+        ctx.channel.send.assert_awaited_once()

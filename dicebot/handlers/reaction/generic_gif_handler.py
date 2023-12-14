a24b2ffcf52d245f2aee533fab9c72316c13efcc
Logan Gore <lgore93@gmail.com>
2023-12-13 19:18:48 -0800

    Turbo everything

--- dicebot/handlers/reaction/generic_gif_handler.py
+++ dicebot/handlers/reaction/generic_gif_handler.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import datetime
 import logging
+
 from dicebot.commands import giffer
 from dicebot.data.types.message_context import MessageContext
 from dicebot.handlers.reaction.abstract_reaction_handler import AbstractReactionHandler
@@ -58,11 +60,28 @@ class GenericGifReactionHandler(AbstractReactionHandler):
 
         logging.info(f"Calling handle for reaction {ctx.reaction}")
 
+        # Check if this was *really* funny
+        current_date = datetime.datetime.now()
+        now = current_date.replace(tzinfo=datetime.timezone.utc)
+        elapsed = now - ctx.reaction.message.created_at
+        is_turbo = elapsed.total_seconds() <= ctx.guild.turbo_reaction_threshold
+
+        if is_turbo:
+            emojis = {e.name: f"<:{e.name}:{e.id}>" for e in ctx.client.emojis}
+            turbo = ["T_", "U_", "R_", "B_", "O_"]
+            turbo_str = "".join(emojis[s] for s in turbo)
+            name = [f"{char}_" for char in ctx.reaction.emoji.name.upper()]
+            name_str = "".join(emojis[s] for s in name)
+            turbo_msg = f"{turbo_str} {name_str}"
+            await ctx.quote_reply(turbo_msg)
+
         # First check this server's custom reactions
         handlers = await ctx.guild.get_all_reaction_handlers(ctx.session)
         for handler in handlers:
             if handler.reaction_equal(ctx.reaction):
-                logging.info(f"Using custom reaction logic for {ctx.reaction}: {handler.gif_search}")
+                logging.info(
+                    f"Using custom logic for {ctx.reaction}: {handler.gif_search}"
+                )
                 gif_url = await giffer.get_random_gif_url(handler.gif_search)
                 if gif_url is not None:
                     await ctx.quote_reply(gif_url)

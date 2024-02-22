0478511170d2c679ab6d9c0d0c845d05861625ef
Logan Gore <lgore93@gmail.com>
2024-02-21 22:52:26 -0800

    Silence non-turbo gif reactions

--- dicebot/handlers/reaction/generic_gif_handler.py
+++ dicebot/handlers/reaction/generic_gif_handler.py
@@ -10,18 +10,6 @@ from dicebot.handlers.reaction.abstract_reaction_handler import AbstractReaction
 
 
 class GenericGifReactionHandler(AbstractReactionHandler):
-    def __init__(self) -> None:
-        self.triggers = {
-            "biden_nice": "Thanks, Obama",
-            "cool_dab": "Cool dab",
-            "deletethis": "Delete this",
-            "livelaughlove": "live laugh love",
-            "ok_boomer": "OK boomer",
-            "this_tbh": "This is true",
-            "press_f": "Press F to pay respects",
-            "question": "Bro what",
-        }
-
     @property
     def reaction_name(self) -> str:
         # Not relevant with this handler
@@ -39,14 +27,10 @@ class GenericGifReactionHandler(AbstractReactionHandler):
             return False
 
         assert ctx.reaction is not None
-        # First check this server's custom reactions
+        # Check this server's custom reactions
         handlers = await ctx.guild.get_all_reaction_handlers(ctx.session)
         for handler in handlers:
             if handler.reaction_equal(ctx.reaction):
-                return True
-        # Then check the defaults
-        for reaction_name in self.triggers:
-            if self.is_emoji_with_name(ctx.reaction, reaction_name):
                 return True
         return False
 
@@ -78,7 +62,7 @@ class GenericGifReactionHandler(AbstractReactionHandler):
             turbo_msg = f"{turbo_str} {name_str}"
             await ctx.quote_reply(turbo_msg, silent=False)
 
-        # First check this server's custom reactions
+        # Check this server's custom reactions
         handlers = await ctx.guild.get_all_reaction_handlers(ctx.session)
         for handler in handlers:
             if handler.reaction_equal(ctx.reaction):
@@ -87,13 +71,7 @@ class GenericGifReactionHandler(AbstractReactionHandler):
                 )
                 gif_url = await giffer.get_random_gif_url(handler.gif_search)
                 if gif_url is not None:
-                    await ctx.quote_reply(gif_url, silent=False)
+                    await ctx.quote_reply(gif_url)
                 # Regardless of whether we sent a quote reply, return here
                 # because otherwise we may get an IndexError below for a bad lookup.
                 return
-
-        # Then check the defaults
-        q = self.triggers[ctx.reaction.emoji.name.lower()]
-        gif_url = await giffer.get_random_gif_url(q)
-        if gif_url is not None:
-            await ctx.quote_reply(gif_url, silent=False)

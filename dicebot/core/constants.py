38f2c953c3bf508564d9db3755be1cd1a059911f
Logan Gore <lgore93@gmail.com>
2026-04-09 21:50:36 -0700

    Remove web app

--- dicebot/core/constants.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-
-DEFAULT_WEB_URL = "localhost"
-WEB_URL_KEY = "DICEBOT_WEB_URL"
--- dicebot/web/constants.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-
-DISCORD_USER_ID_SESSION_KEY = "discord_user_id"
-DISCORD_PROVIDER_VALUES_ID_KEY = "id"
